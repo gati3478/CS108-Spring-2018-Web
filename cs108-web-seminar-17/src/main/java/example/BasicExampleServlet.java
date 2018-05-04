dfe223f8bcdb47ac0c5fc9d769f920554985c37b
gati3478 <giopetriashvili@gmail.com>
2018-05-04 23:16:10 +0400

    Minor

--- cs108-web-seminar-17/src/main/java/example/BasicExampleServlet.java
+++ cs108-web-seminar-17/src/main/java/example/BasicExampleServlet.java
@@ -1,5 +1,8 @@
+package example;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,29 +11,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class FormServlet
+ * Servlet implementation class example.BasicExampleServlet
  */
-@WebServlet(value = "/FormServlet", name = "FormServlet")
-public class FormServlet extends HttpServlet {
+@WebServlet(value = "/example.BasicExampleServlet", name = "example.BasicExampleServlet")
+public class BasicExampleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public FormServlet() {
+    public BasicExampleServlet() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
 
@@ -41,16 +39,16 @@ public class FormServlet extends HttpServlet {
         out.println("<title>A Basic Servlet Example</title>");
         out.println("</head>");
         out.println("<body>");
-
-        out.println("<h1>Servlet Form Example</h1>");
-
-        out.println("The Message for Today is ");
-        out.println("<b><u>");
-        out.println(request.getParameter("data"));
-        out.println("</u></b>");
-
+        out.println("It is currently " + new Date());
         out.println("</body>");
         out.println("</html>");
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
     }
 
 }
--- cs108-web-seminar-17/src/main/java/example/FormServlet.java
+++ cs108-web-seminar-17/src/main/java/example/FormServlet.java
@@ -1,6 +1,7 @@
+package example;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,24 +10,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class BasicExampleServlet
+ * Servlet implementation class example.FormServlet
  */
-@WebServlet(value = "/BasicExampleServlet", name = "BasicExampleServlet")
-public class BasicExampleServlet extends HttpServlet {
+@WebServlet(value = "/example.FormServlet", name = "example.FormServlet")
+public class FormServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public BasicExampleServlet() {
+    public FormServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
 
@@ -37,16 +43,16 @@ public class BasicExampleServlet extends HttpServlet {
         out.println("<title>A Basic Servlet Example</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("It is currently " + new Date());
+
+        out.println("<h1>Servlet Form Example</h1>");
+
+        out.println("The Message for Today is ");
+        out.println("<b><u>");
+        out.println(request.getParameter("data"));
+        out.println("</u></b>");
+
         out.println("</body>");
         out.println("</html>");
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
     }
 
 }
